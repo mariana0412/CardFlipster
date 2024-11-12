46a3a5d5a0ffc2c5f3e5dda8f7b5db340ea54ef9
Mariana Piz <marianapiz0412@gmail.com>
2024-11-12 21:50:07 +0200

    Add flashcard model

--- Sources/CardFlipster/CardFlipster.swift
+++ Sources/CardFlipster/CardFlipster.swift
@@ -1,1 +1,17 @@
+public struct Flashcard  {
+    public let text: String
+    public let answer: String
 
+    
+    
+    
+    
+    
+    
+    
+    
+    public init(text: String, answer: String) {
+        self.text = text
+        self.answer = answer
+    }
+}
